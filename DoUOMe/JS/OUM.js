f5039750a979de8eca00ed18b274f4f49c47cad5
PVDat <dat6a6zz@gmail.com>
2019-08-02 10:02:27 +0700

    Ứng dụng vui: Do You Love Me

---
+++ DoUOMe/JS/OUM.js
@@ -0,0 +1,9 @@
+function yeS() {
+    alert("Chuẩn rồi đó! IB6YR");
+}
+function nowayOut(){
+    let x =Math.round(Math.random() * window.innerHeight);
+    let y =Math.round(Math.random() * window.innerWidth);
+    document.getElementById("nO").style.left = x + 'px';
+    document.getElementById("nO").style.top = y + 'px';
+}
